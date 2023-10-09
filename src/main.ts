f9b4ea9950fbf1b0b4b659b6b287fdbea82ea9ac
serhiipopov <sergiiy.popov@gmail.com>
2023-10-09 09:47:33 +0300

    connect to db

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { PrismaService } from './database/prisma.service';
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule);
+
+	app.setGlobalPrefix('api');
+	app.enableCors();
+	await app.listen(3000);
+}
+
+bootstrap();
